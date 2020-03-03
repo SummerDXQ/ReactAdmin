9e8ac53cfe289e46f0409d2d853cef3f01871753
XiaoqianDing <dxq3442@gmail.com>
2020-03-04 09:38:15 +1100

    header

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,31 @@
 // APT request function
 import ajax from './ajax'
+import jsonp from 'jsonp'
+import { message } from 'antd'
+import axios from 'axios'
 
 // Login
 export const reqLogin = (username,password)=>  ajax('/login',{username,password},'POST')
 // Add user
 export const reqUser = (user)=>  ajax('/manage/user/add',user,'POST')
+
+// jsonp, weather
+// export const reqWeather = (city)=>{
+//     return new Promise((resolve,reject)=>{
+//         const url = `http://api.weatherapi.com/v1/current.json?key=4f1daaa213ba487e87892150200303&q=${city}`
+//         // jsonp(url,{param: 'callback'},(error, response)=>{
+//         //     console.log('jsonp')
+//         //     console.log(response)
+//         //     // if(!error && data.status === 'success'){
+//         //     //     const {dayPictureUrl,weather} = data.results[0].weather_data[0]
+//         //     //     resolve({dayPictureUrl,weather})
+//         //     // }else{
+//         //     //     message.error('Require weather information failed')
+//         //     // }
+//         // })
+        
+//     })
+// }
+
+// 4f1daaa213ba487e87892150200303
+// reqWeather('sydney')
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,94 @@
 import React,{Component} from 'react'
 import './header.less'
+import {formateDate} from '../../utils/dateUtils'
+import memoryUtils from '../../utils/memoryUtils'
+import storageUtils from '../../utils/storageUtils'
+import {reqWeather} from '../../api/index'
+import {withRouter} from 'react-router-dom'
+import menulist from '../../config/menuConfig'
+import Item from 'antd/lib/list/Item'
+import { Modal } from 'antd';
+import LinkButton from '../link-button/link-button'
 
-export default class Header extends Component{
+class Header extends Component{
+    state = {
+        currentTime:formateDate(Date.now()),
+        dayPictureUrl:'',
+        weather:''
+    }
+
+    getTime=()=>{
+        this.intervalID = setInterval(()=>{
+            const currentTime = formateDate(Date.now())
+            this.setState({
+                currentTime
+            })
+        },1000)
+    }
+
+    getWeather = async ()=>{
+        // const {}
+    }
+
+    getTitle = () => {
+        const path = this.props.location.pathname
+        let title
+        menulist.forEach((item)=>{
+            if(item.key===path){
+                title = item.title
+            }else if(item.children){
+                const cItem = item.children.find(cItem=> path === cItem.key)
+                if(cItem){
+                    title = cItem.title
+                }
+            }
+        })
+        return title
+    }
+
+    logout = () =>{
+        console.log('dianji')
+        Modal.confirm({
+            content:'Do you want to logout',
+            onOk:()=>{
+                // console.log("ok")
+                storageUtils.removeUser()
+                memoryUtils.username={}
+                this.props.history.replace('/login')
+            }
+        })
+    }
+
+    componentWillMount(){
+        this.getTime()
+        // this.getWeather()
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.intervalID )
+    }
     render(){
+        const {currentTime,dayPictureUrl,weather} = this.state;
+        const username = memoryUtils.user.username
+        const title = this.getTitle()
         return(
             <div className="header">
-                Header
+                <div className="header-top">
+                <span>Welcome,{username}</span>
+                    {/* <a href="javaSript:" onClick={this.logout}>Logout</a> */}
+                    <LinkButton onClick={this.logout}>Logout</LinkButton>
+                </div>
+                <div className="header-bottom">
+                <div className="header-bottom-left">{title}</div>
+                    <div className="header-bottom-right">
+                        <span>{currentTime}</span>
+                        <img src="http://api.map.baidu.com/images/weather/day/qing.png" alt=""/>
+                        <span>Windy</span>
+                    </div>
+                </div>
             </div>
         )
     }
 }
+
+export default withRouter(Header)
